5ea5a87bd5b35aee0f6f80906c1f9ce752f7b6c5
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2023-12-26 01:45:50 +0600

    card can be changed by rank and suit data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-// import { useState } from 'react';
 import './App.css';
 import Card from "./component/card";
+
 
 function App() {
 
   return (
     <>
-      <Card rank="9" suit="♦"/>
+      <Card rank="k" suit="hearts"/>
     </>
   )
 }
--- src/component/card.tsx
+++ src/component/card.tsx
@@ -1,16 +1,28 @@
-import React from "react";
-
 interface Props{
     suit: string,
     rank: string,
 }
-
 const Card:React.FC<Props> = (props)=>{
+    let cardSymbol:string = "";
+    switch (props.suit){
+        case "hearts":
+            cardSymbol = "♥";
+            break;
+        case "diams":
+            cardSymbol = "♦";
+            break;
+        case "clubs":
+            cardSymbol = "♣";
+            break;
+        case "spades":
+            cardSymbol = "♠";
+            break;
+    }
     return(
         <div className= "playingCards faceImages">
-         <span className={"card diams " + "rank-"+ props.rank }>
+         <span className={"card " + props.suit  + " rank-"+ props.rank}>
           <span className="rank">{props.rank}</span>
-          <span className="suit">{props.suit}</span>
+          <span className="suit">{cardSymbol}</span>
         </span>
         </div>
     )
